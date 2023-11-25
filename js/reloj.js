7b82c6f91015206038d4c97e3d8e16e33a48845e
CosmeNicolas <nicomas7590@gmail.com>
2023-11-25 15:16:34 -0300

    inicializamos el proyecto reloj

---
+++ js/reloj.js
@@ -0,0 +1,8 @@
+/* Crear una web con un reloj que muestre la siguiente información: dia, hora , año
+minutos y segundos */
+
+const nuevoreloj = () =>{
+  let fecha = new Date()
+  console.log(fecha)
+}
+
