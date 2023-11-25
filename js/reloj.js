bbd73dd4440182549228c537a27b324f73f85a6e
CosmeNicolas <nicomas7590@gmail.com>
2023-11-25 15:41:58 -0300

    tomamos los elementos del metodo Date

--- js/reloj.js
+++ js/reloj.js
@@ -1,8 +1,17 @@
 /* Crear una web con un reloj que muestre la siguiente información: dia, hora , año
 minutos y segundos */
 
-const nuevoreloj = () =>{
-  let fecha = new Date()
-  console.log(fecha)
-}
 
+const datos = Date()
+console.log(datos)
+
+const reloj = ()=>{
+  horas = Date.getHours(),
+  ampm,
+  minutos = Date.getMinutes(),
+  segundos = Date.getSeconds(),
+  dia = Date.getDay(),
+  diaSemana = Date.getDay(),
+  mes = Date.getMonth(),
+  anio = Date.getFullYeaDate
+}
